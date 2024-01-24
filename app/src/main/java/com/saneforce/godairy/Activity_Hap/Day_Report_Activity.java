55e3af5d41efc3d82b6bd5e214e3226f90c667fe
usha <friendusha9@gmail.com>
2024-01-24 18:02:40 +0530

    Day Report - Added Adapter and Model class

--- app/src/main/java/com/saneforce/godairy/Activity_Hap/Day_Report_Activity.java
+++ app/src/main/java/com/saneforce/godairy/Activity_Hap/Day_Report_Activity.java
@@ -1,17 +1,25 @@
 package com.saneforce.godairy.Activity_Hap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.gson.Gson;
 import com.saneforce.godairy.Common_Class.Common_Class;
@@ -20,6 +28,7 @@ import com.saneforce.godairy.Interface.AdapterOnClick;
 import com.saneforce.godairy.Interface.UpdateResponseUI;
 import com.saneforce.godairy.Model_Class.DayReport;
 import com.saneforce.godairy.R;
+import com.saneforce.godairy.adapters.DateReportAdapter;
 import com.saneforce.godairy.adapters.DayReportAdapter;
 import com.saneforce.godairy.common.DatabaseHandler;
 import com.saneforce.godairy.databinding.ActivityDayReportBinding;
@@ -32,15 +41,11 @@ import java.util.List;
 
 public class Day_Report_Activity extends AppCompatActivity {
     private ActivityDayReportBinding binding;
-    public  static  String mDate = "";
+    public static String mDate = "";
     private DatePickerDialog datePickerDialog;
     com.saneforce.godairy.Activity_Hap.Common_Class DT = new com.saneforce.godairy.Activity_Hap.Common_Class();
-    private  TextView tvDate,tvSFADate,tvdaydate,txtDate,tvempname,tvsalerepname,thempid_desi,tvtime,
-            tvvisitdis,tvvisitoutlet,tvorderdist,tvorderoutlet,
-            tvordereddist,tvorderedoutlet,tvinvoicedist,tvinvoiceoutlet;
+    BottomSheetBehavior bottomSheetBehavior;
 
-    Button button;
-    RecyclerView day_report_recycler,  rvdayreportrecycler;
     DayReportAdapter dayReportAdapter;
     BottomSheetDialog sheetDialog;
     Gson gson;
@@ -48,6 +53,9 @@ public class Day_Report_Activity extends AppCompatActivity {
     DatabaseHandler db;
     Shared_Common_Pref shared_common_pref;
     List<DayReport> Dayreport = new ArrayList<>();
+    private float initialX, initialY;
+
+    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,44 +65,105 @@ public class Day_Report_Activity extends AppCompatActivity {
 
         shared_common_pref = new Shared_Common_Pref(Day_Report_Activity.this);
         common_class = new Common_Class(this);
+        initialX =  binding.bottomAnc.getX();
+        initialY =  binding.bottomAnc.getY();
 
         initViews();
 
-        tvdaydate.setText(DT.getDateWithFormat(new Date(), "dd-MMM-yyyy"));
-        binding.viewSummary.setOnClickListener(v -> {
-            sheetDialog=new BottomSheetDialog(Day_Report_Activity.this,R.style.AppBottomSheetDialogTheme) ;
-            sheetDialog.setContentView(R.layout.day_bottom_sheet);
-            sheetDialog.show();
-        });
+
+
     }
 
+    private void initBottomSheet() {
+        sheetDialog = new BottomSheetDialog(Day_Report_Activity.this, R.style.AppBottomSheetDialogTheme);
+        sheetDialog.getBehavior().toString();
+        sheetDialog.setContentView(R.layout.day_bottom_sheet);
+        sheetDialog.show();
+    }
+
+
     private void initViews() {
-        tvSFADate = findViewById(R.id.tvSFADate);
-        tvdaydate = findViewById(R.id.txtdayDate);
-        tvempname =  findViewById(R.id.emp_name);
-        tvsalerepname =  findViewById(R.id.work_type);
-        thempid_desi =  findViewById(R.id.emp_id_deg);
-        tvtime =  findViewById(R.id.submit_time);
-        tvvisitdis =  findViewById(R.id.visited_distributor);
-        tvvisitoutlet =  findViewById(R.id.visited_outlet);
-        tvorderdist =  findViewById(R.id.order_distributor);
-        tvvisitoutlet =  findViewById(R.id.order_outlet);
-        tvordereddist =  findViewById(R.id.ordered_distributor);
-        tvorderedoutlet =  findViewById(R.id.ordered_outlet);
-        tvinvoicedist =  findViewById(R.id.invoiced_distributor);
-        tvinvoiceoutlet =  findViewById(R.id.invoiced_outlet);
+
+        binding.bottomAnc.setOnClickListener(v -> {
+
+            ObjectAnimator animation = ObjectAnimator.ofFloat(binding.bottomAnc, "translationY", -100f);
+            animation.setDuration(200);
+            animation.start();
+            final Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                onTouch();
+                binding.bottomAnc.setVisibility(View.GONE);
+                initBottomSheet();
+             //   handler.removeCallbacks(Day_Report_Activity.this);
+            }, 500);
+
+        });
+        // Start animation
+    /*    binding.bottomAnc.setOnClickListener(v -> {
+                    binding.bottomAnc.startAnimation(slide_down);
+
+                    if (binding.bottomAnc.getVisibility() == View.GONE) {
+                        Animation.ABSOLUTE.slideDown(binding.bottomAnc);
+                        binding.bottomAnc.setVisibility(View.VISIBLE);
+
+                    } else {
+                        binding.bottomAnc.setVisibility(View.GONE);
+                        ViewAnimatorSlideUpDown.slideUp(binding.bottomAnc);
+                    }
+                }*/
+        binding.txtdayDate.setText(DT.getDateWithFormat(new Date(), "dd-MMM-yyyy"));
+      /*  binding.bottomAnc.setOnClickListener(v -> {
+            Animation slide_up = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+            slide_up.start();
+            sheetDialog = new BottomSheetDialog(Day_Report_Activity.this, R.style.AppBottomSheetDialogTheme);
+            sheetDialog.getBehavior().toString();
+            sheetDialog.setContentView(R.layout.day_bottom_sheet);
+            sheetDialog.show();
+        });*/
+
+
+        //Load animation
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_down);
+
+        Animation slide_up = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_up);
+
+
 
         binding.txtdayDate.setOnClickListener(v -> {
             Calendar newCalendar = Calendar.getInstance();
             datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
                 public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                     int month = monthOfYear + 1;
-                    txtDate.setText("" + year + "-" + month + "-" + dayOfMonth);
-                    mDate = tvDate.getText().toString();
+
+                    date = ("" + year + "-" + month + "-" + dayOfMonth).toString();
+                    binding.txtdayDate.setText(date);
+
                 }
             }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
             datePickerDialog.show();
         });
+
+
+      /*  bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomAnc);
+        return bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    //Log.e("BottomSheet", "Expanded");
+                } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    //Log.e("BottomSheet", "Collapsed");
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }*/
+        }
+
+    private void onTouch() {
     }
 }
 
