403f57d9162f5376a66667cb9f32132fa3994471
anbu <anbu@saneforce.com>
2024-02-26 22:27:57 +0530

    merge 26feb View invoices List

--- app/src/main/java/com/saneforce/godairy/SFA_Activity/vwInvoices.java
+++ app/src/main/java/com/saneforce/godairy/SFA_Activity/vwInvoices.java
@@ -2,15 +2,23 @@ package com.saneforce.godairy.SFA_Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.saneforce.godairy.R;
 
 public class vwInvoices extends AppCompatActivity {
+    TextView txtPONo,txtIndentNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vw_invoices);
+        Intent i=getIntent();
+        txtIndentNo=findViewById(R.id.txtIndentNo);
+        txtIndentNo.setText(i.getExtras().getString("PONO"));
+        txtPONo=findViewById(R.id.txtIndentNo);
+        txtPONo.setText(i.getExtras().getString("salno"));
     }
 }
